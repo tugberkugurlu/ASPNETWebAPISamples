0088bee8d0eeb15e12ee158cf25c2137594431d6
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-02-23 16:23:34 +0200

    initial commit

---
+++ ElmahErrorFilterApp/src/ElmahErrorFilterApp/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using ElmahErrorFilterApp.Infrastructure.Filters;
+
+namespace ElmahErrorFilterApp {
+
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication {
+
+        protected void Application_Start() {
+
+            Configure(System.Web.Http.GlobalConfiguration.Configuration);
+        }
+
+        private void Configure(HttpConfiguration httpConfiguration) {
+
+            httpConfiguration.Filters.Add(new ElmahErrorAttribute());
+
+            httpConfiguration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Never;
+
+            httpConfiguration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ElmahErrorFilterApp/src/ElmahErrorFilterApp/Infrastructure/Filters/ElmahErrorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElmahErrorFilterApp.Infrastructure.Filters {
+
+    public class ElmahErrorAttribute : 
+        System.Web.Http.Filters.ExceptionFilterAttribute {
+
+        public override void OnException(
+            System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext) {
+
+            if(actionExecutedContext.Exception != null)
+                Elmah.ErrorSignal.FromCurrentContext().Raise(actionExecutedContext.Exception);
+
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
