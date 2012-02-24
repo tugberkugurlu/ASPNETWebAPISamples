62e1618286eae7844c81b16fe8aaccacb53e59d3
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-02-24 15:34:11 +0200

    added OAuthMessageHandler sample

---
+++ OAuthMessageHandler/src/OAuthMessageHandler.Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Json;
+using System.Threading.Tasks;
+
+namespace OAuthMessageHandler.Client {
+
+    class Program {
+
+        static string _address =
+            "http://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=tourismgeek&count=5";
+
+        static void Main(string[] args) {
+
+            // Create client and insert an OAuth message handler in the message path that
+            // inserts an OAuth authentication header in the request
+            HttpClient _client = new HttpClient(
+                new OAuthMessageHandler.Core.OAuthMessageHandler(
+                    new HttpClientHandler()
+                )
+            );
+
+            #region _w/o await
+
+            //_client.GetAsync(_address).ContinueWith(requestTask => {
+
+            //    // Get HTTP response from completed task.
+            //    HttpResponseMessage response = requestTask.Result;
+
+            //    // Check that response was successful or throw exception
+            //    response.EnsureSuccessStatusCode();
+
+            //    // Read response asynchronously as JsonValue and write out tweet texts
+            //    response.Content.ReadAsAsync<JsonArray>().ContinueWith(readTask => {
+
+            //        JsonArray statuses = readTask.Result;
+            //        Console.WriteLine("\nLast 5 statuses from ScottGu's twitter account:\n");
+            //        foreach (var status in statuses) {
+
+            //            Console.WriteLine(status["text"] + "\n");
+            //        }
+
+            //    });
+
+            //});
+
+            #endregion
+
+            #region _with await
+
+            readResponseAsync(_client);
+
+            #endregion
+
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        private static async Task<HttpResponseMessage> getResponseAsync(HttpClient httpClient) {
+
+            var response = await httpClient.GetAsync(_address);
+
+            // Check that response was successful or throw exception
+            response.EnsureSuccessStatusCode();
+
+            return response;
+        }
+
+        private static async void readResponseAsync(HttpClient httpClient) {
+
+            var response = await getResponseAsync(httpClient);
+            var statuses = await response.Content.ReadAsAsync<JsonArray>();
+
+            Console.WriteLine("\nLast 5 statuses from ScottGu's twitter account:\n");
+            foreach (var status in statuses) {
+
+                Console.WriteLine(status["text"] + "\n");
+            }
+        }
+    }
+}
---
+++ OAuthMessageHandler/src/OAuthMessageHandler.Core/OAuthMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using OAuth;
+using System.Threading.Tasks;
+
+namespace OAuthMessageHandler.Core {
+
+    public class OAuthMessageHandler : DelegatingHandler {
+
+        private static string _consumerKey = System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"];
+        private static string _consumerSecret = System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"];
+        private static string _token = System.Configuration.ConfigurationManager.AppSettings["AccessToken"];
+        private static string _tokenSecret = System.Configuration.ConfigurationManager.AppSettings["AccessTokenSecret"];
+
+        private OAuthBase _oauthBase = new OAuthBase();
+
+        public OAuthMessageHandler(HttpMessageHandler innerHandler) 
+            : base(innerHandler) { }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken) {
+
+            // Compute OAuth header
+            string normalizedUri;
+            string normalizedParameters;
+            string authHeader;
+
+            string signature = _oauthBase.GenerateSignature(
+                request.RequestUri,
+                _consumerKey,
+                _consumerSecret,
+                _token,
+                _tokenSecret,
+                request.Method.Method,
+                _oauthBase.GenerateTimeStamp(),
+                _oauthBase.GenerateNonce(),
+                out normalizedUri,
+                out normalizedParameters,
+                out authHeader);
+
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("OAuth", authHeader);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
