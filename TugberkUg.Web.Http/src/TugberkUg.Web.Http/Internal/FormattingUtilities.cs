b3d91c1843fe2b1985062a15e081387e593ce8bb
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-03-03 02:10:17 +0200

    added TugberkUg.Web.Http project with RouteDataMediaTypeMapping, CSVMediaTypeFormatter, JSONPMediaTypeFormatter, etc.

---
+++ TugberkUg.Web.Http/src/TugberkUg.Web.Http/Internal/FormattingUtilities.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Json;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+
+//Taken from System.Net.Http Library
+namespace TugberkUg.Web.Http.Internal {
+
+    internal static class FormattingUtilities {
+
+        public const string HttpRequestedWithHeader = "x-requested-with";
+        public const string HttpRequestedWithHeaderValue = "xmlhttprequest";
+        public const string JsonNullLiteral = "null";
+        public const string HttpHostHeader = "Host";
+        public const string HttpVersionToken = "HTTP";
+        public static readonly Type Utf8EncodingType = typeof(UTF8Encoding);
+        public static readonly Type Utf16EncodingType = typeof(UnicodeEncoding);
+        public static readonly Type HttpRequestMessageType = typeof(HttpRequestMessage);
+        public static readonly Type HttpResponseMessageType = typeof(HttpResponseMessage);
+        public static readonly Type HttpContentType = typeof(HttpContent);
+        public static readonly Type DelegatingEnumerableGenericType = typeof(DelegatingEnumerable<>);
+        public static readonly Type EnumerableInterfaceGenericType = typeof(IEnumerable<>);
+        public static readonly Type QueryableInterfaceGenericType = typeof(IQueryable<>);
+        public static readonly Type JsonValueType = typeof(JsonValue);
+        private static readonly HashSet<string> httpContentHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase) {
+
+			"Allow", 
+			"Content-Disposition", 
+			"Content-Encoding", 
+			"Content-Language", 
+			"Content-Length", 
+			"Content-Location", 
+			"Content-MD5", 
+			"Content-Range", 
+			"Content-Type", 
+			"Expires", 
+			"Last-Modified"
+		};
+        public static HashSet<string> HttpContentHeaders
+        {
+            get
+            {
+                return FormattingUtilities.httpContentHeaders;
+            }
+        }
+        public static bool IsJsonValueType(Type type)
+        {
+            return FormattingUtilities.JsonValueType.IsAssignableFrom(type);
+        }
+        public static HttpContentHeaders CreateEmptyContentHeaders()
+        {
+            HttpContent httpContent = null;
+            HttpContentHeaders httpContentHeaders = null;
+            try
+            {
+                httpContent = new StringContent(string.Empty);
+                httpContentHeaders = httpContent.Headers;
+                httpContentHeaders.Clear();
+            }
+            finally
+            {
+                if (httpContent != null)
+                {
+                    httpContent.Dispose();
+                }
+            }
+            return httpContentHeaders;
+        }
+        public static bool ValidateCollection(Collection<MediaTypeHeaderValue> actual, MediaTypeHeaderValue[] expected)
+        {
+            if (actual.Count != expected.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < expected.Length; i++)
+            {
+                MediaTypeHeaderValue item = expected[i];
+                if (!actual.Contains(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static string UnquoteToken(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return token;
+            }
+            if (token.StartsWith("\"", StringComparison.Ordinal) && token.EndsWith("\"", StringComparison.Ordinal) && token.Length > 1)
+            {
+                return token.Substring(1, token.Length - 2);
+            }
+            return token;
+        }
+    }
+}
---
+++ TugberkUg.Web.Http/src/TugberkUg.Web.Http/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TugberkUg.Web.Http")]
+[assembly: AssemblyDescription("TugberkUg's Library For ASP.NET Web API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tugberk Ugurlu")]
+[assembly: AssemblyProduct("TugberkUg.Web.Http")]
+[assembly: AssemblyCopyright("Copyright © Tugberk Ugurlu 2012")]
+[assembly: AssemblyTrademark("Tugberk Ugurlu")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("41333f90-b4fd-4b2a-905b-ea6d905b698a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.*")]
