0c3735a1d014211778eaea670cf9bc8a03f38cce
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-03-04 00:04:34 +0200

    added ValidationAttribute

---
+++ TugberkUg.Web.Http/src/TugberkUg.Web.Http/Filters/ValidationAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace TugberkUg.Web.Http.Filters {
+
+    public class ValidationAttribute : ActionFilterAttribute {
+
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+
+            var modelState = actionContext.ModelState;
+
+            if (!modelState.IsValid) {
+
+                dynamic errors = new JsonObject();
+
+                foreach (var key in modelState.Keys) {
+
+                    var state = modelState[key];
+
+                    if (state.Errors.Any())
+                        errors[key] = state.Errors.First().ErrorMessage;
+                }
+
+                actionContext.Response = new HttpResponseMessage<JsonValue>(errors, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
