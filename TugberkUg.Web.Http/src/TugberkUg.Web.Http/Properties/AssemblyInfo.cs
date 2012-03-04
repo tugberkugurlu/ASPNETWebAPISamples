ba06e597adbdb6724105f1db52b3a83059b45559
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-03-04 23:41:47 +0200

    implemented SemVer for nuget packages on TugberkUg.Web.Http proj

--- TugberkUg.Web.Http/src/TugberkUg.Web.Http/Properties/AssemblyInfo.cs
+++ TugberkUg.Web.Http/src/TugberkUg.Web.Http/Properties/AssemblyInfo.cs
@@ -32,3 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("0.1.1-preview")]
