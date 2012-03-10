8d28ca3e8c6639e3ea182aa01cceccb8b2ee8888
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-03-10 11:48:56 +0200

    added a message handler and TugberkUg.Web.Http.Tests proj

---
+++ TugberkUg.Web.Http/src/TugberkUg.Web.Http/MessageHandlers/TraceLoggingHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace TugberkUg.Web.Http.MessageHandlers {
+
+    //https://github.com/thinktecture/Thinktecture.Web.Http/blob/master/Thinktecture.Web.Http/Handlers/LoggingHandler.cs
+    public class TraceLoggingHandler : DelegatingHandler {
+
+        protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, System.Threading.CancellationToken cancellationToken) {
+
+            Trace.TraceInformation("Begin Request: {0} {1}", request.Method, request.RequestUri);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
