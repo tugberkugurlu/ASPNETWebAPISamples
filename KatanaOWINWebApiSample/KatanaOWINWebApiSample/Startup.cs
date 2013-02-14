bee61a4548c122d8aa40286d94fda8530a8191b3
tugberkugurlu <tugberkugurlu@gmail.com>
2013-02-15 00:03:54 +0200

    added KatanaOWINWebApiSample

---
+++ KatanaOWINWebApiSample/KatanaOWINWebApiSample/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Owin;
+using System.Threading;
+using System.Web.Http;
+
+namespace KatanaOWINWebApiSample {
+    
+    public class Startup {
+
+        public void Configuration(IAppBuilder builder) {
+
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultHttpRoute",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+            
+            builder.UseHttpServer(config);
+        }
+    }
+}
