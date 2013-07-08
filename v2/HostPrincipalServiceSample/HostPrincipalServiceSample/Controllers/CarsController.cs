5d129915284e9112f543e762bd5fa3678cde35b8
tugberkugurlu <tugberkugurlu@gmail.com>
2013-07-08 14:06:44 +0300

    completed HostPrincipalServiceSample

---
+++ v2/HostPrincipalServiceSample/HostPrincipalServiceSample/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace HostPrincipalServiceSample.Controllers
+{
+    [Authorize]
+    public class CarsController : ApiController
+    {
+        public string[] GetCars()
+        {
+            return new[]
+            {
+                "Cars 1",
+                "Cars 2"
+            };
+        }
+    }
+}
---
+++ v2/HostPrincipalServiceSample/HostPrincipalServiceSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HostPrincipalServiceSample.Global" Language="C#" %>
