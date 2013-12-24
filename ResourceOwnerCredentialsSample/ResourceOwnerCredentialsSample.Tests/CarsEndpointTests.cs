7a63bc31024f253e8e24599ebba95b01c0d86f60
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-12-24 14:04:07 +0200

    running the tests in memory with OAuth2Client

--- ResourceOwnerCredentialsSample/ResourceOwnerCredentialsSample.Tests/CarsEndpointTests.cs
+++ ResourceOwnerCredentialsSample/ResourceOwnerCredentialsSample.Tests/CarsEndpointTests.cs
@@ -16,18 +16,17 @@ namespace ResourceOwnerCredentialsSample.Tests
         [Fact]
         public async Task Should_Call_The_Cars_Endpoint_With_A_Valid_Access_Token()
         {
-            //using (TestServer server = TestServer.Create<Startup>())
-            //{
-                OAuth2Client client = new OAuth2Client(new Uri("http://localhost.fiddler:18008/token"));
+            using (TestServer server = TestServer.Create<Startup>())
+            {
+                OAuth2Client client = new OAuth2Client(new Uri("http://localhost.fiddler:18008/token"), server.Handler);
                 TokenResponse tokenResponse = await client.RequestResourceOwnerPasswordAsync("bob", "bob");
 
-                HttpClient client2 = new HttpClient();
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost.fiddler:18008/api/cars");
                 request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
-                HttpResponseMessage response = await client2.SendAsync(request);
+                HttpResponseMessage response = await server.HttpClient.SendAsync(request);
 
                 Assert.True(response.IsSuccessStatusCode);
-            //}
+            }
         }
     }
 }
