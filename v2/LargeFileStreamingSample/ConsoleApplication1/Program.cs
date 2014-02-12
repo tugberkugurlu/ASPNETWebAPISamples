1c769b8b9c90e1afc7cb3354c1d9d2e4ef5e7641
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-02-12 15:25:18 +0200

    added large file streaming sample

---
+++ v2/LargeFileStreamingSample/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ref: http://stackoverflow.com/questions/12533533/async-reading-chunked-content-with-httpclient-from-asp-net-webapi
+            // ref: http://stackoverflow.com/questions/16998/reading-chunked-response-with-httpwebresponse
+
+            // HttpGetForLargeFileInWrongWay();
+            // HttpGetForLargeFileInRightWay();
+        }
+
+        static void HttpGetForLargeFileInWrongWay()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:22381");
+                HttpResponseMessage response = client.GetAsync("/api/file").Result;
+                using (Stream streamToReadFrom = response.Content.ReadAsStreamAsync().Result)
+                {
+                    string fileToWriteTo = Path.GetTempFileName();
+                    using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
+                    {
+                        streamToReadFrom.CopyTo(streamToWriteTo);
+                    }
+                }
+            }
+        }
+
+        static void HttpGetForLargeFileInRightWay()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:22381");
+                HttpResponseMessage response = client.GetAsync("/api/file", HttpCompletionOption.ResponseHeadersRead).Result;
+                using (Stream streamToReadFrom = response.Content.ReadAsStreamAsync().Result)
+                {
+                    string fileToWriteTo = Path.GetTempFileName();
+                    using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
+                    {
+                        streamToReadFrom.CopyTo(streamToWriteTo);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ v2/LargeFileStreamingSample/WebApplication7/Controllers/FileController.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication7.Controllers
+{
+    public class FileController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            Stream streamToReadFrom = File.OpenRead(@"C:\Users\Tugberk\AppData\Local\Temp\tmpEE38.tmp");
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(streamToReadFrom);
+
+            return response;
+        }
+    }
+}
