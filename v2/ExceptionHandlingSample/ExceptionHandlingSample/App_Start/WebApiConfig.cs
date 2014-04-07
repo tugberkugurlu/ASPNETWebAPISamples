6476b38f1d88c71b6084b1887875c3f724996f97
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-04-07 20:17:30 +0300

    added exceptionhandlingsample

---
+++ v2/ExceptionHandlingSample/ExceptionHandlingSample/App_Start/WebApiConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+
+namespace ExceptionHandlingSample
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Services.Replace(typeof(IExceptionHandler), new MyGlobalHandler());
+        }
+    }
+
+    public class MyGlobalHandler : ExceptionHandler
+    {
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            const string Message = "Your input conflicted with the current state of the system.";
+            if (context.Exception is DivideByZeroException)
+            {
+                context.Result = new ResponseMessageResult(context.Request.CreateErrorResponse(HttpStatusCode.Conflict, Message));
+            }
+            else
+            {
+                HttpError error = new HttpError("An interval server error occured.");
+                error.Add("CorrelationId", context.Request.GetCorrelationId().ToString());
+                var response = context.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, error);
+                context.Result = new ResponseMessageResult(response);
+            }
+        }
+
+        public override bool ShouldHandle(ExceptionHandlerContext context)
+        {
+            return true;
+        }
+    }
+}
---
+++ v2/ExceptionHandlingSample/ExceptionHandlingSample/Controllers/CarsController.cs
@@ -0,0 +1,97 @@
+﻿using ExceptionHandlingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Filters;
+using System.Web.Http.Results;
+
+namespace ExceptionHandlingSample.Controllers
+{
+    [SpecialToCarsControllerExceptionFilter]
+    public class CarsController : ApiController
+    {
+        CarsContext _carsContext = new CarsContext();
+
+        public CarsController()
+        {
+           // throw new DivideByZeroException();
+        }
+
+        public IEnumerable<Car> Get()
+        {
+            throw new InvalidOperationException();
+            return _carsContext.All;
+        }
+
+        public Car Get(int id)
+        {
+            var car = _carsContext.GetSingle(x => x.Id == id);
+
+            if (car == null)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Contact not found")
+                };
+
+                throw new HttpResponseException(response);
+            }
+
+            return car;
+        }
+
+        public HttpResponseMessage Post(Car car)
+        {
+
+            _carsContext.Add(car);
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+        public Car Put(int id, Car car)
+        {
+            car.Id = id;
+            if (!_carsContext.TryUpdate(car))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Car not found")
+                };
+
+                throw new HttpResponseException(response);
+            }
+
+            return car;
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            if (!_carsContext.TryRemove(id))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Contact not found")
+                };
+
+                throw new HttpResponseException(response);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+
+    public class SpecialToCarsControllerExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception is DivideByZeroException)
+            {
+                actionExecutedContext.Response = actionExecutedContext.Request.CreateErrorResponse(HttpStatusCode.Conflict, "Your input conflicted with the current state of the system.");
+            }
+        }
+    }
+}
