4a1ac0cb30870fb8e5ffb92b541516a874941be2
tugberkugurlu <tugberkugurlu@gmail.com>
2014-07-07 15:00:51 +0300

    added AspNetTimeoutSample

---
+++ v2/AspNetTimeoutSample/AspNetTimeoutSample/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Web.Http;
+
+namespace AspNetTimeoutSample.Controllers
+{
+    public class CarsController : ApiController
+    {
+        public string[] GetCars(int waitFor)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(waitFor));
+
+            return new[]
+            {
+                "Car 1",
+                "Car 2",
+                "Car 3"
+            };
+        }
+    }
+}
---
+++ v2/AspNetTimeoutSample/AspNetTimeoutSample/MessageHandlers/AspNetTimeoutHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspNetTimeoutSample.MessageHandlers
+{
+    public class AspNetTimeoutHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpContext.Current.Server.ScriptTimeout = 20;
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
